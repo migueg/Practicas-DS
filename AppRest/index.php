72b4bf65097c25ecf01cbeeacadca6b0437400e5
migueg <miguegarciatenorio@gmail.com>
2020-05-09 21:03:02 +0200

    inicio cliente web

---
+++ AppRest/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<htm>
+    <head>
+        <meta charset="utf-8">
+        <title>RolGame</title>
+        <link rel="shortcut icon" href ="./img/hacha.png" type="image/png"/>
+
+    </head>
+    <body>
+
+    </body>
+</htm>
